50785d935d73904d193ca76772a797d0db759efe
Nipunayt <nipunanamal698@gmail.com>
2025-07-22 22:02:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function checkLogin(event) {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
-  if (username === "kave-md" && password === "kavee") {
+  if (username === "nipuna-md" && password === "nipuna") {
     document.querySelector('.login-box').style.opacity = '0';
     document.querySelector('.login-box').style.transform = 'translate(-50%, -50%) scale(0.9)';
     setTimeout(() => {
@@ -12,6 +12,6 @@ function checkLogin(event) {
       document.querySelector('.business-page').style.display = 'block';
     }, 1000);
   } else {
-    alert("❌ වැරදි Username හෝ Password");
+    alert("🧐 වැරදි Username හෝ Password");
   }
 }
